565c93cbe0df4f3c2d2554d6adec189e23bcb1f4
Xos <157793201+xostc@users.noreply.github.com>
2025-02-26 12:36:06 +0000

    Tu mensaje de commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://xostc.github.io/xostc.github.io',
+    site: 'https://xostc.github.io',
     integrations: [
         mdx(),
         sitemap(),
